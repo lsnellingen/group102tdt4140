098fed069885c032a5ec43b8264a3ea861d746df
Hansch <hans_c40@hotmail.com>
2017-02-24 09:44:28 +0100

    New login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,45 @@
 // Libs
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { PageHeader } from 'react-bootstrap';
+
+import Button from 'react-bootstrap/lib/Button'
+import Panel from 'react-bootstrap/lib/Panel'
+import { FormControl, Checkbox } from 'react-bootstrap';
+
 
 class Login extends Component {
 
   render() {
+    const title = 'Log In';
     return (
-      <div className="row">
-        <PageHeader>THIS IS THE LOGIN SITE</PageHeader>
-        <div className="col-xs-6 col-xs-offset-3">
-            <form>
-              <div className="form-group">
-                <label for="exampleInputEmail1">Email</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-              </div>
-              <button type="submit" className="btn btn-default" disabled>Submit</button>
-            </form>
+      <div className="App">
+        <div className="text-center">
+          <h1 className="login-brand-text">eduBOT</h1>
+          <h3 className="text-color">An automatic feedback systems</h3>
+
         </div>
+
+        <Panel header={<h3>Please Sign In</h3>} className="login-panel">
+          <div className="form-group">
+            <FormControl
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              name="name"
+            />
+          </div>
+          <div className="form-group">
+              <FormControl
+                className="form-control"
+                placeholder="Password"
+                type="password"
+                name="password"
+              />
+          </div>
+          <Checkbox label="Remember Me" > Remember Me </Checkbox>
+          <Button type="submit" bsSize="large" bsStyle="success" block>Login</Button>
+          <Button type="submit" bsSize="large" bsStyle="danger" block>Login</Button>
+        </Panel>
         <div className="col-md-6 col-md-offset-3 m-t-30">
           <Link to="/lecturer"><button className="btn btn-primary btn-lg" type="button">Lecturer</button></Link>
           <Link to="/student"><button className="btn btn-primary btn-lg" type="button">Student</button></Link>
