113fa24cc2a5c1e1542841a0eb09bbe4e2c3b294
lsnellingen <larssnellingen@hotmail.com>
2017-03-15 14:11:23 +0100

    Added content areas on the feedback page

--- src/js/pages/ViewFeedbackPage.js
+++ src/js/pages/ViewFeedbackPage.js
@@ -13,10 +13,44 @@ export default class ViewFeedbackPage extends React.Component {
 		              <hr />
 	        		</div>
 	        	</div>
-	        	<div className="row">
-		            <div className="col-xs-12">
-		            	
+	        	<div className="form-group">
+		        	<div className="row">
+			            <div className="col-xs-12">
+			            	<textarea type="feedback" className="form-control" id="feedback" name="feedback" placeholder="Choose subject and lecture to view feecback... " required />
+			            </div>
 		            </div>
+	            </div>
+	            <div className="form-group">
+	            	<div className="row">
+		            	<div className="col-xs-12 col-sm-12">
+				            <p>Choose the subject to view feedback: </p>
+				            <select type="subject-selector" className="form-control" id="subject-selector" name="subject-selector">
+							  <option value="chooseSubject">Choose subject</option>
+							  <option value="subject1">Subject 1</option>
+							  <option value="subject2">Subject 2</option>
+							  <option value="subject3">Subject 3</option>
+							  <option value="subject4">Subject 4</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div className="form-group">
+	            	<div className="row">
+		            	<div className="col-xs-12 col-sm-12">
+				            <p>Choose lecture in chosen subject: </p>
+				            <select type="lecture-selector" className="form-control" id="lecture-selector" name="lecture-selector">
+				              <option value="chooseLecture">Choose lecture</option>
+							  <option value="subject1">Lecture 1</option>
+							  <option value="subject2">Lecture 2</option>
+							  <option value="subject3">Lecture 3</option>
+							  <option value="subject4">Lecture 4</option>
+							</select>
+						</div>
+					</div>
+				</div>
+
+	            <div className="form-group">
+		            	<button type="button" className="btn btn-primary">View feedback</button>
 	            </div>
         	</div>
   		</DocumentTitle>
